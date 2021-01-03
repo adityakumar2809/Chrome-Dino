5ef7b233b36a85171a4dd688ad2835de7a8a6d39
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-04 00:17:52 +0530

    Take Screenshots using pillow

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import pyautogui
 from PIL import Image, ImageGrab
 
 
+def takeScreenshot():
+    image = ImageGrab.grab()
+    image.show()
+
+
 if __name__ == "__main__":
-    pass
+    takeScreenshot()
