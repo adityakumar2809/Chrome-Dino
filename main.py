838103109f3564f6e13a94f96e5fdcc594873934
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-04 22:13:54 +0530

    Add sky clearance code

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ def takeScreenshot():
     return image
 
 
-def drawOnImage(image, crop_dimensions):
+def drawOnImage(image, ground_crop_dimensions, sky_crop_dimensions):
     pixel_object = image.load()
-    for i in range(crop_dimensions[0], crop_dimensions[2]):
-        for j in range(crop_dimensions[1], crop_dimensions[3]):
-            pixel_object[i, j] = (255,0,0)
+    for i in range(ground_crop_dimensions[0], ground_crop_dimensions[2]):
+        for j in range(ground_crop_dimensions[1], ground_crop_dimensions[3]):
+            if (i - ground_crop_dimensions[0] < 10) or \
+                (ground_crop_dimensions[2] - i < 10) or \
+                (j - ground_crop_dimensions[1] < 10) or \
+                    (ground_crop_dimensions[3] - j < 10):
+
+                pixel_object[i, j] = (255, 0, 0)
+
+    for i in range(sky_crop_dimensions[0], sky_crop_dimensions[2]):
+        for j in range(sky_crop_dimensions[1], sky_crop_dimensions[3]):
+            if (i - sky_crop_dimensions[0] < 10) or \
+                (sky_crop_dimensions[2] - i < 10) or \
+                (j - sky_crop_dimensions[1] < 10) or \
+                    (sky_crop_dimensions[3] - j < 10):
+
+                pixel_object[i, j] = (0, 0, 255)
     image.show()
 
-def isGroundObstacleDetected(image):
+
+def isObstacleDetected(image):
     pixel_object = image.load()
     for i in range(image.size[0]):
         for j in range(image.size[1]):
@@ -31,15 +46,20 @@ def hitKeyboard(key):
 
 if __name__ == "__main__":
     debug = False
-    crop_dimensions = (250, 650, 375, 700)
+    ground_crop_dimensions = (250, 650, 375, 700)
+    sky_crop_dimensions = (250, 595, 375, 645)
     if debug:
         image = takeScreenshot()
-        drawOnImage(image, crop_dimensions)
+        drawOnImage(image, ground_crop_dimensions, sky_crop_dimensions)
     else:
         print('Dino game commences in 3 seconds...')
         time.sleep(3)
         hitKeyboard('up')
         while True:
-            image = ImageGrab.grab(bbox=crop_dimensions).convert('L')
-            if isGroundObstacleDetected(image):
+            image = ImageGrab.grab(bbox=ground_crop_dimensions).convert('L')
+            if isObstacleDetected(image):
                 hitKeyboard('up')
+                continue
+            image = ImageGrab.grab(bbox=sky_crop_dimensions).convert('L')
+            if isObstacleDetected(image):
+                hitKeyboard('down')
