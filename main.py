72b1139b9f08a5a54492c293e5090fcae95bc939
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-04 22:01:43 +0530

    Ensure ground object clearance is maintained

--- main.py
+++ main.py
@@ -5,40 +5,41 @@ from PIL import Image, ImageGrab
 
 
 def takeScreenshot():
-    image = ImageGrab.grab().convert('L')
+    image = ImageGrab.grab()
     return image
 
 
-def drawOnImage(image):
+def drawOnImage(image, crop_dimensions):
     pixel_object = image.load()
-    for i in range(275, 300):
-        for j in range(675, 700):
-            pixel_object[i, j] = 0
+    for i in range(crop_dimensions[0], crop_dimensions[2]):
+        for j in range(crop_dimensions[1], crop_dimensions[3]):
+            pixel_object[i, j] = (255,0,0)
     image.show()
 
 def isGroundObstacleDetected(image):
     pixel_object = image.load()
-    for i in range(275, 300):
-        for j in range(675, 700):
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
             if pixel_object[i, j] > 100:
                 return True
     return False
 
 
 def hitKeyboard(key):
-    pyautogui.keyDown(key)
+    pyautogui.press(key)
 
 
 if __name__ == "__main__":
     debug = False
+    crop_dimensions = (250, 650, 375, 700)
     if debug:
         image = takeScreenshot()
-        drawOnImage(image)
+        drawOnImage(image, crop_dimensions)
     else:
         print('Dino game commences in 3 seconds...')
         time.sleep(3)
         hitKeyboard('up')
         while True:
-            image = takeScreenshot()
+            image = ImageGrab.grab(bbox=crop_dimensions).convert('L')
             if isGroundObstacleDetected(image):
                 hitKeyboard('up')
